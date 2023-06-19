e5b8abeeb8f4f8e0eca07b9d69437a2ad7d75d46
jaisreera <8479946428ll@gmail.com>
2023-06-19 09:35:39 +0530

    intial commite

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
-mongoose.connect('mongodb://localhost:27017/ecommerceDB', {
+mongoose.connect('mongodb+srv://rahulkumar:bjENbINKJ1DPnz5f@e-comerce-api.btfinea.mongodb.net/E-comerce-API?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
-
+// bjENbINKJ1DPnz5f
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
